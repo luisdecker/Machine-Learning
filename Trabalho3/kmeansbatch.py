2d63a8050360052164ad8b7cc08c1cc8d2216717
Luis Decker <luisgustavo.decker@gmail.com>
2017-11-08 13:36:18 -0200

    Resultados kmeans

--- Trabalho3/kmeansbatch.py
+++ Trabalho3/kmeansbatch.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from docOps import Document
 from sklearn.neighbors import NearestNeighbors
+import operator
 
 
 # In[ ]:
@@ -27,12 +28,13 @@ class KMeans_Batch:
         self.docs = docs
         
     def knn(self,k,point):
-        neigh = NearestNeighbors(n_neighbors=k,algorithm = 'ball_tree')
+        neigh = NearestNeighbors(n_neighbors=k,algorithm = 'ball_tree',n_jobs=-1,leaf_size=60)
         neigh.fit(self.raw_data)
         return neigh.kneighbors(point.reshape(1,-1),return_distance=False)
       
     def run(self,k):
         #Treinamos o k-means
+        print("Iniciando KMeans [{}]".format(k))
         kmeans = KMeans(
             n_clusters=k,
             n_jobs=-1,
@@ -42,6 +44,7 @@ class KMeans_Batch:
             max_iter=self.iterations,
             algorithm=self.km_algorithm)
         kmeans.fit(self.raw_data)
+        print("Terminado KMeans [{}]".format(k))
         #Separamos os documentos por classes
         doc_classes = {}
         file_index=0
@@ -53,13 +56,15 @@ class KMeans_Batch:
         #Verificamos os 10 vizinhos mais próximos de cada centroide
         centroids_knn = {}
         centroids = kmeans.cluster_centers_
+        print("Iniciando KNN [{}]".format(k))
         for cluster in range(k):
             centroids_knn[cluster] = self.knn(10,centroids[cluster])
-        
+        print("Terminado KNN [{}]".format(k))
         #Salvamos o resultado num arquivo
         #Iremos salvar o Newsgroups dos 10nn de cada 
         results = open(file="ResultsKnn"+str(k)+".txt",mode='w')
         #print(centroids_knn)
+        print("Iniciando gravação [{}]".format(k))
         for cluster in range(k):
             ten_nn = centroids_knn[cluster] #indice dos 10nn
             ten_nn = ten_nn[0]
@@ -81,9 +86,15 @@ class KMeans_Batch:
                 print(classes,file=results)
             
             print ("\n______ALL______\n",file=results)
-            all_groups = set()
+            all_groups = {}
             for doc in doc_classes[cluster]:
-                all_groups = all_groups.union(doc.groups)
-            print(all_groups,file=results)
+                for group in doc.groups:
+                    if group not in all_groups.keys():
+                        all_groups[group] = 0
+                    all_groups[group] += 1
+            sorted_groups = sorted(all_groups.items(),key=operator.itemgetter(1),reverse=True)
+            for group in sorted_groups:
+                print("{} = {}".format(group[0],group[1]),file=results)
+        print("Terminado gravação [{}]".format(k))
 
 
